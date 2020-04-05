38aa6ec31b4cc05fd9d7dd22a6f8d85685d7645b
Austin <mathfreak65@gmail.com>
2020-04-05 13:41:50 +0000

    Fix loading list tab with no lists.
    The list editability should default to false if no list has been loaded.
    Also, when no list has been loaded, the tab should not be clickable.

--- qmlist/templates/js/shopping-list.js
+++ qmlist/templates/js/shopping-list.js
@@ -3,33 +3,34 @@ function _shoppingListEditability(isEditable) {
 }
 
 function setShoppingListEditability(shoppingListName) {
+    if (shoppingListName === undefined || shoppingListName === null) {
+        _shoppingListEditability(false);
+    } else {
+        $.get("{{ url_for('load_shopping_list') }}", {"shopping-list": shoppingListName})
+            .done(function(data) {
+                _shoppingListEditability(data["editable"]);
+            })
+            .fail(function(jqXHR, textStatus) {
+                alert(textStatus);
+            });
+        }
+}
+
+function loadShoppingList(shoppingListName) {
+    setListTabEnabled(shoppingListName);
     $.get("{{ url_for('load_shopping_list') }}", {"shopping-list": shoppingListName})
         .done(function(data) {
             _shoppingListEditability(data["editable"]);
+
+            displayItems(data, shoppingListName, true);
         })
         .fail(function(jqXHR, textStatus) {
             alert(textStatus);
         });
 }
 
-function loadShoppingList(shoppingListName) {
-    if (shoppingListName === undefined || shoppingListName === null) {
-        $("#list-tab").addClass("disabled");
-    } else {
-        $("#list-tab").removeClass("disabled");
-
-        $.get("{{ url_for('load_shopping_list') }}", {"shopping-list": shoppingListName})
-            .done(function(data) {
-                _shoppingListEditability(data["editable"]);
-
-                displayItems(data, shoppingListName, true);
-            })
-            .fail(function(jqXHR, textStatus) {
-                alert(textStatus);
-            });
-    }
-}
-
 $("#list-tab").click(function() {
-     loadShoppingList($("#list-tab").attr("data-list-name"));
+    if (!$("#list-tab").hasClass("disabled")) {
+        loadShoppingList($("#list-tab").attr("data-list-name"));
+    }
 });
